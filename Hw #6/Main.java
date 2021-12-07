a414c8fd2b6fc31cdec55ba5a0fc161c0a035dc2
Riley-Heaman <93946712+Riley-Heaman@users.noreply.github.com>
2021-12-06 23:04:25 -0500

    Add files via upload

---
+++ Hw #6/Main.java
@@ -0,0 +1,12 @@
+//Riley Heaman
+//CIS 219 Tue, Thur @3:30
+//HW #6: For loop
+public class Main {
+    public static void main(String args[]){
+        //init the for loop (variable to loop on; break condition; increment)
+        for(int i = 0; i <= 10; i++){
+            //print the value of i.
+            System.out.println(i);
+        }
+    }
+}
